5277640695faf7e28ab064d63a9b3b97725a7578
yuvrajiro <yuvrajiro@gmail.com>
2026-01-06 21:44:49 +0530

    New Version for PyPI: 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="kmnet",
-    version="0.1.0",
+    version="0.1.1",
     author="Yuvraj",
     author_email="yuvraj@gmail.com",
     description="A discrete-time survival analysis model with Kaplan-Meier inspired loss.",
